5776eff832240524bd6af92c0c4d706bc6233cf8
Arbaz0001 <arbaz0001@gmail.com>
2026-02-13 22:31:35 +0530

    updates

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -9,16 +9,18 @@ import { AdminAuthProvider } from "./context/AdminAuthContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AdminAuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/admin">
         <Routes>
-          {/* Default redirect when visiting "/" */}
-          <Route path="/" element={<Navigate to="/admin/login" replace />} />
 
-          {/* Admin login page */}
-          <Route path="/admin/login" element={<AdminLogin />} />
+          {/* Default when visiting /admin */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
 
-          {/* Admin dashboard & all other admin routes */}
-          <Route path="/admin/*" element={<App />} />
+          {/* Login page -> /admin/login */}
+          <Route path="/login" element={<AdminLogin />} />
+
+          {/* All other admin routes */}
+          <Route path="/*" element={<App />} />
+
         </Routes>
       </BrowserRouter>
     </AdminAuthProvider>
